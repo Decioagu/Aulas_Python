0fe51b918df561f0a5719397c2d86426e5ec0eeb
DECIO SANTANA DE AGUIAR <decioagu@gmail.com>
2024-10-03 10:08:11 -0300

    ajuste

--- Aula_sys/modulo/meu_modulo.py
+++ Aula_sys/modulo/meu_modulo.py
@@ -1,2 +1,2 @@
 def impotar_modulo():
-    print("\nExecutando modulo externo no diretório: Aula_sys\modulo\meu_modulo.py\n")
+    print("\n>>>>> Executando modulo externo no diretório: Aula_sys\modulo\meu_modulo.py <<<<<\n")
