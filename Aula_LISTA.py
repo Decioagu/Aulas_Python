a3cee1112ee7b10723c9d91a9b297ded3be75383
DECIO SANTANA DE AGUIAR <decioagu@gmail.com>
2025-06-19 18:09:46 -0300

    19/06/25

--- Aula_LISTA.py
+++ Aula_LISTA.py
@@ -482,13 +482,17 @@ lista2 = ['BA', 'SP', 'MG', 'RJ']
 lista3 = []
 
 zip_object = zip(lista1, lista2) # retorna um objeto Map
-# zip_object = list(zip(lista1, lista2))
+print(zip_object) # <zip object at 0x000001A2B3C4D5E6>
 
 for item in zip_object:
     lista3.append(item)
 
-print(lista3)
-# print(zip_object)
+print(lista3) # (('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG'))
+
+# OU
+
+lista3 = [valor for valor in zip_object]  
+print(lista3) # (('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG'))
 
 print()
 print('========================')
